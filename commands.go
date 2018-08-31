d349976b6143bf1c642b1082e295afcb07f3a3af
Matt Moriarity <matt@mattmoriarity.com>
2018-08-31 09:33:52 -0500

    Use semaphore to prevent multiple check outs

--- commands.go
+++ commands.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"github.com/nlopes/slack"
 	"github.com/travis-ci/vsphere-images"
+	"golang.org/x/sync/semaphore"
 )
+
+var hostSemaphore = semaphore.NewWeighted(1)
 
 func IsHostCheckedOut(ctx context.Context, msg *slack.MessageEvent) {
 	typing(ctx)
@@ -35,6 +38,13 @@ func CheckOutHost(ctx context.Context, msg *slack.MessageEvent) {
 		reply(ctx, "Looks like there's already a host checked out for building images!")
 		return
 	}
+
+	canCheckOut := hostSemaphore.TryAcquire(1)
+	if !canCheckOut {
+		reply(ctx, ":exclamation: Someone is already trying to check in/out a host right now, try again later!")
+		return
+	}
+	defer hostSemaphore.Release(1)
 
 	// Choosing a host can take a little time, so this message makes the bot more responsive
 	reply(ctx, "Choosing a host to check out…")
@@ -72,6 +82,13 @@ func CheckInHost(ctx context.Context, msg *slack.MessageEvent) {
 		reply(ctx, "Looks like there isn't a host checked out right now!")
 		return
 	}
+
+	canCheckOut := hostSemaphore.TryAcquire(1)
+	if !canCheckOut {
+		reply(ctx, ":exclamation: Someone is already trying to check in/out a host right now, try again later!")
+		return
+	}
+	defer hostSemaphore.Release(1)
 
 	reply(ctx, "Checking the host back into the production cluster...")
 	host, err := vsphereimages.CheckInHost(ctx, vSphereURL, vSphereInsecure, packerClusterPath, prodClusterPath, newProgressLogger())
